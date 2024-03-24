fa12d1dca209e2e74315fbc6959f7769c5dc1d75
11812310 <e11812310@student.tuwien.ac.at>
2024-03-24 17:21:49 +0100

    scene recognition instead of frame recognition as agreed upon, tested & working

--- recognition_app.py
+++ recognition_app.py
@@ -55,47 +55,56 @@ def vid_recognise(query: Query) -> Answer:
                     return True
             return False
 
-    logfile_name = f"logfile_app_processed_{query.vid_name}"
+    logfile_name = f"logfile_app_processed_scene_{query.vid_name}"
     for person in query.persons:
         logfile_name += f"_{person}"
     log = open(f"{logfile_name}.txt", "w") 
 
     processed_frames = []
     i = 0
-
-    # for each frame:
+    timestamp_last_recognition = -5000
+    timestamp_last_detection = -2000
+    last_detection_no = 0
+    
+    # for each scene
     while(capture.isOpened()):
 
         retval, frame = capture.read()
-        timestamp = capture.get(cv2.CAP_PROP_POS_MSEC)
-
         if retval:
-            frame_cpy = frame.copy()
-            # detect faces
-            results_frame = model(frame)
-            # for each box = detected face:
-            j = 0
-            for box in results_frame[0].boxes:
-                # save cropped face as image
-                image_path = Path(f'./temp/frame{i}_face{j}.jpg')
-                save_one_box(box.xyxy, frame, file=Path(f'./temp/frame{i}_face{j}.jpg'), BGR=True)
-                j += 1
-                # TODO: swap for a real time signature if that part will work 
-                if recognise_face(timestamp, image_path, query.persons) == True:
-                    marked_frame = mark_box(box, frame_cpy)
-                    processed_frames.append(marked_frame)
-                else:
-                    processed_frames.append(frame)  
+            timestamp = capture.get(cv2.CAP_PROP_POS_MSEC)
+            processed_frame = frame.copy()
+            # if 2s have passed since the last detection
+            if (timestamp - timestamp_last_detection > 2000 or timestamp - timestamp_last_recognition > 5000):
+                    timestamp_last_detection = timestamp
+                    # detect faces
+                    results_frame = model(frame)
+                    print(len(results_frame[0].boxes))
+                    # if the number of detected faces has changed since last detection or if 5s have passed since last recognition
+                    if (len(results_frame[0].boxes) != last_detection_no or timestamp - timestamp_last_recognition > 5000):
+                        last_detection_no = len(results_frame[0].boxes)
+                        timestamp_last_recognition = timestamp
+                        # for each box = detected face:
+                        j = 0
+                        for box in results_frame[0].boxes:
+                            # save cropped face as image
+                            image_path = Path(f'./temp/frame{i}_face{j}.jpg')
+                            save_one_box(box.xyxy, frame, file=Path(f'./temp/frame{i}_face{j}.jpg'), BGR=True)
+                            j += 1
+                            if recognise_face(timestamp, image_path, query.persons) == True:
+                                processed_frame = mark_box(box, processed_frame)
         else:
             break
-        # testing condition, just for n first frames   
-        if i > 200:
+        
+        processed_frames.append(processed_frame)
+        
+        # testing condition, just for i first frames   
+        if i > 1000:
             break
 
         i += 1
             
     fps = capture.get(cv2.CAP_PROP_FPS)
-    procesessed_video_name = f"processed_app_{query.vid_name}"
+    procesessed_video_name = f"processed_app_scene_{query.vid_name}"
     for person in query.persons:
         procesessed_video_name += f"_{person}"
     procesessed_video_name += ".mp4"
